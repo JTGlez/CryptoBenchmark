526477db5db295d19b970b1cc6ced5c4a4ef1147
JTGlez <jorjeluigi1@comunidad.unam.mx>
2023-04-15 14:54:45 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#Importación de bibliotecas.
+import subprocess
+import sys
+import runpy
+
+#Instalación de paquetes en el environment de ejecución.
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+#Instalación de bibliotecas adicionales.
+install('pycryptodome')
+install('matplotlib')
+install('ecdsa')
+install('cryptography')
+
+#Ejecución de los scripts.
+subprocess.call(['python','./run_CipherDecipher.py'])
+subprocess.call(['python','./run_Hashing.py'])
+subprocess.call(['python','./run_Signature.py'])
