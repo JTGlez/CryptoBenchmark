545e77a6b8c55f6c5d95799bb13c389a520e1098
JTGlez <jorjeluigi1@comunidad.unam.mx>
2023-04-15 16:03:52 -0600

    Preeliminar RSA-PSS

--- Signing/RSA_PSS.py
+++ Signing/RSA_PSS.py
@@ -2,74 +2,54 @@
 from Crypto.Signature import pss
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
-from Crypto import Random
 
 #----------------------PROCESO DE GENERACIÓN Y EXPORTACIÓN DE CLAVES-------------------------#
+def generate_keys():
+    """Genera el par de claves (pública y privada) para el firmado con RSA-PSS. """
+
+    # Generación del par de claves.
+    key = RSA.generate(2048)
+
+    # Passphrase para cifrar la clave privada generada.
+    frase = "ProyectoCrypto123_!ñ"
+
+    # Exportación de la clave privada al archivo private.pem. Abre el archivo private.pem, y lo crea si es que no existe.
+    private_key = key.export_key(passphrase = frase)
+    with open("private.pem", "wb") as f:
+        f.write(private_key)
+    
+    # Exportación de la clave pública al archivo public.pem. Abre el archivo public.pem, y lo crea si es que no existe.
+    public_key = key.publickey().export_key()
+    with open("public.pem", "wb") as f:
+        f.write(public_key)
+
+    return frase
+
+#---------------------------PROCESO DE FIRMADO Y VERIFICACIÓN---------------------------------#
 
 def signing(file):
     """Realiza el proceso de firma y verificación de la firma sobre un archivo vector de prueba."""
 
+    # Se obtiene el par de claves con la función generate_keys y sus correspondientes archivos en formato .pem. Se recupera la passphrase.
+    frase = generate_keys()
 
+    # Apertura del archivo a firmar en formato binario.
+    with open(file, "rb") as imageFile:
+        archivo = imageFile.read()
 
+    # Proceso de firmado con la clave privada con cálculo de hash.
+    privatekey = RSA.import_key(open('private.pem').read(), frase)
+    hash = SHA256.new(archivo)
+    firma = pss.new(privatekey).sign(hash)
 
+    # Proceso de verificación de la firma con la clave pública del firmante.
+    publickey = RSA.import_key(open('public.pem').read())
+    hash = SHA256.new(archivo)
+    verificador = pss.new(publickey)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def guardarKeys(private_key, public_key):
-    """Guarda el par de claves generadas en ficheros en formato .pem"""
-    with open("private.pem", "wb") as f:
-        f.write(private_key)
-    with open("public.pem", "wb") as f:
-        f.write(public_key) 
-
-#Generación de pareja de claves RSA de 2048 bits. Se usa un vector de inicialización.
-random_generator = Random.new().read
-key = RSA.generate(2048, random_generator)
-
-#Passphrase para el cifrado de la clave privada.
-passphrase = "12345"
-
-#Exportación de la clave privada.
-private_key = key.export_key(passphrase=passphrase, pkcs=8, protection="scryptAndAES128-CBC")
-
-#Cálculo de la clave pública.
-public_key = key.publickey().export_key()
-
-
-#----------------------------PROCESO DE FIRMADO Y VERIFICACIÓN--------------------------------#
-#Definición del mensaje a verificar.
-message = b'To be signed'
-#Recuperamos la clave privada del archivo de seguridad.
-key_private = RSA.import_key(open('private.pem').read(), passphrase)
-#Cálculo de un hash para el firmado con SHA-256.
-hash = SHA256.new(message)
-#Firmado del mensaje.
-signature = pss.new(key_private).sign(hash)
-
-#Leemos la claves publica generada previamente, obtenemos el hash del mensaje a verificar.
-key_public = RSA.import_key(open('public.pem').read(), passphrase)
-hash = SHA256.new(message)
-verifier = pss.new(key_public)
-
-#Try-Catch para verificar si la firma es válida. Si no, lanza una excepción.
-try:
-    verifier.verify(hash, signature)
-    print("La firma del archivo es válida.")
-except (ValueError, TypeError):
-    print("La firma del archivo es incorrecta y ha sido alterada.")
+    # Verificación de firma legítima o apócrifa.
+    try:
+        verificador.verify(hash, firma)
+        print ("Firma auténtica y validada.")
+    except (ValueError, TypeError):
+        print ("Firma inválida.")
