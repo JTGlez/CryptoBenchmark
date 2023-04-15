29a8718baf8a508ee2142b924129bddea40630c4
JTGlez <jorjeluigi1@comunidad.unam.mx>
2023-04-15 17:54:50 -0600

    Test exitoso ED25519

--- Signing/run_Signature.py
+++ Signing/run_Signature.py
@@ -1,10 +1,10 @@
-import RSA_PSS, ECDSA_P521
+import RSA_PSS, ECDSA_P521, ED25519
 import matplotlib.pyplot as plt
 import time
 
 archivo = 'TestVectors/text.txt'
 
-def benchmarkRSA_PSS():
+def benchmark_RSA_PSS():
     """Invoca la función de firmado RSA_PSS sobre un archivo vector de prueba y mide el tiempo de prueba."""
     inicio = time.time()
     firmado = RSA_PSS.signing(archivo)
@@ -12,7 +12,7 @@ def benchmarkRSA_PSS():
     tiempo_RSA_PSS = (fin - inicio)
     return tiempo_RSA_PSS
 
-def benchmarkECDSA521():
+def benchmark_ECDSA521():
     """Invoca la función de firmado ECDSA - NIST P-521  sobre un archivo vector de prueba y mide el tiempo de prueba."""
     inicio = time.time()
     firmado = ECDSA_P521.signing(archivo)
@@ -20,8 +20,18 @@ def benchmarkECDSA521():
     tiempo_ECDSA_P521 = (fin - inicio)
     return tiempo_ECDSA_P521
 
-rsa_pss_time = benchmarkRSA_PSS()
-ecdsa_p521_time = benchmarkECDSA521()
+def benchmark_ED25519():
+    """Invoca la función de firmado ECDSA - NIST P-521  sobre un archivo vector de prueba y mide el tiempo de prueba."""
+    inicio = time.time()
+    firmado = ED25519.signing(archivo)
+    fin = time.time()
+    tiempo_ECDSA_P521 = (fin - inicio)
+    return tiempo_ECDSA_P521
+
+rsa_pss_time = benchmark_RSA_PSS()
+ecdsa_p521_time = benchmark_ECDSA521()
+ed_25519_time = benchmark_ED25519()
 
 print("Tiempo de RSA-PSS con el vector de prueba: ", rsa_pss_time, "segundos.")
 print("Tiempo de ECDSA_P521 con el vector de prueba: ", ecdsa_p521_time, "segundos.")
+print("Tiempo de ED25519 con el vector de prueba: ", ed_25519_time, "segundos.")
