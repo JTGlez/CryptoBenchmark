7fedadda139a0690cb9da0c784bf252beaf79083
JTGlez <jorjeluigi1@comunidad.unam.mx>
2023-04-15 19:46:48 -0600

    etc

--- run_Benchmark.py
+++ run_Benchmark.py
@@ -4,10 +4,16 @@
 #          -Cruz Rangel Leonardo Said
 #          -Téllez González Jorge Luis
 
-# Importación de bibliotecas.
+#Importación de bibliotecas.
 from Signing import run_Signature
-from CipherDeciphering.results_generator import generate_results
-from CipherDeciphering.plotter import plot_results
+import subprocess
+import matplotlib.pyplot as plt
+from run_encrypt_decrypt import generate_results
+from plotter import plot_results
+
+def addlabels(x,y):
+    for i in range(len(x)):
+        plt.text(i, y[i]//2, y[i], ha = 'center')
 
 archivos_prueba = ['file.pdf', 'img.PNG', 'text.txt']
 num_executions = 5
@@ -16,5 +22,3 @@ for file in archivos_prueba:
     print(f"Procesando archivo {file}")
     labels, encryption_times, decryption_times = generate_results(file, num_executions)
     plot_results(file, labels, encryption_times, decryption_times, num_executions)
-
-signing_times = run_Signature.signingTime()
