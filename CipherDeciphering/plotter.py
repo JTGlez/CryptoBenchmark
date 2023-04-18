4d050c68146fc119775cc820adef1ecea0a54d67
David Calderon Jimenez <73145220+Derek533z@users.noreply.github.com>
2023-04-17 18:53:11 -0600

    Update plotter.py

--- CipherDeciphering/plotter.py
+++ CipherDeciphering/plotter.py
@@ -12,14 +12,14 @@ def plot_results(file, labels, encryption_times, decryption_times, num_execution
     for i in range(len(labels)):
         # Graficar tiempos de encriptación
         ax[i].bar(0, encryption_times[i], color=colors[i])
-        ax[i].set_xlabel("Encriptación")
+        ax[i].set_xlabel("Cifrado")
 
         # Graficar tiempos de desencriptación
         ax[i].bar(1, decryption_times[i], color=colors[i])
-        ax[i].set_xlabel("Desencriptación")
+        ax[i].set_xlabel("Descifrado")
 
         ax[i].set_xticks([0, 1])
-        ax[i].set_xticklabels(["Encriptación", "Desencriptación"])
+        ax[i].set_xticklabels(["Cifrado", "Descifrado"])
         ax[i].set_title(f'{labels[i]} para {file_name} ({num_executions} ejecuciones)')
 
     ax[0].set_ylabel("Tiempo promedio (s)")
